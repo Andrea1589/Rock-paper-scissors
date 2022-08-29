0a773cd809287013537c6c58110324fbe47d2085
Andrea1589 <andreaecs@gmail.com>
2022-08-29 17:11:52 -0500

    Create getComputerChoice function and test it

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+//The participant choose one option (rock, paper or scissors) secretly
+function getComputerChoice(){
+    let computerChoice = undefined;
+    let numChoice = getRandomInt(3);
+    console.log(numChoice);
+    switch(numChoice){
+        case 0: 
+            computerChoice = 'rock';
+            break;
+        case 1: 
+            computerChoice = 'paper';
+            break;
+        case 2: 
+            computerChoice = 'scissors';
+            break;
+    }
+
+    return computerChoice;
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
+
+//At the same time the participant show their option to each other
+//The participant who beats the other according the next description wins
+//1- Scissors beats paper
+//2- Paper beats rock
+//3- Rock beats scissors
