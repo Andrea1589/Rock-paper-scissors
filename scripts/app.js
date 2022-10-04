a442eb4770ad90126cbf6a20b6ea05d9560a4057
Andrea Oficina <andreaecs@gmail.com>
2022-10-04 08:52:20 -0500

    Add code to summarize final result and add a message in the DOM

--- scripts/app.js
+++ scripts/app.js
@@ -32,46 +32,57 @@ function getRandomInt(max){
 
 function playRound(roundNumber, playerSelection, computerSelection){
     let resultMessage = undefined;
+    let winner = '';
 
-    let content = document.querySelector('div');
+    let content = document.getElementById('rounds');
     const div = document.createElement('div');
 
     if (playerSelection === 'rock'){
         if(computerSelection === 'paper') {
             resultMessage = 'You Lose! Paper beats Rock';
+            winner = 'computer';
         } else if (computerSelection === 'scissors') {
             resultMessage = 'You Win! Rock beats Scissors';
+            winner = 'player';
         } else {
             resultMessage = 'Your Even! Both chose Rock';
+            winner = 'even';
         }
     } else if (playerSelection === 'paper'){
         if(computerSelection === 'rock') {
             resultMessage = 'You Win! Paper beats Rock';
+            winner = 'player';
         } else if (computerSelection === 'scissors') {
             resultMessage = 'You Lose! Scissors beat Paper';
+            winner = 'computer';
         } else {
             resultMessage = 'Your Even! Both chose Paper';
+            winner = 'even';
         }
     } else {
         if(computerSelection === 'rock') {
             resultMessage = 'You Lose! Rock beats Scissors';
+            winner = 'computer';
         } else if (computerSelection === 'paper') {
             resultMessage = 'You Win! Scissors beat Paper';
+            winner = 'player';
         } else {
             resultMessage = 'Your Even! Both chose Scissors';
+            winner = 'even';
         }
     }
 
     div.textContent = 'Round ' + roundNumber + ': ' + resultMessage;
     content.appendChild(div);
     //console.log('Computer choice: ' + computerSelection + ' Your choice: ' + playerSelection + '. ' + resultMessage);
-    return resultMessage;
-
+    return winner;
 }
 
 function game(maxRounds){
     let countRounds = 0;
     let playerChoice = '';
+    let playerPoints = 0;
+    let computerPoints = 0;
 
     //Add event listener on button options
     document.body.addEventListener('click', function clickButton(event) {
@@ -79,10 +90,29 @@ function game(maxRounds){
             countRounds += 1;
             playerChoice = event.target.textContent.toLowerCase();
             let result = playRound(countRounds, playerChoice, getComputerChoice());
+
+            //Sum points
+            if (result == 'player') {
+                playerPoints += 1;
+            } else {
+                computerPoints += 1;
+            }
            
             //Remove event listener when reach max number of rounds
             if (countRounds == maxRounds) {
                 document.body.removeEventListener('click', clickButton);
+
+                let content = document.getElementById('result')
+                const div = document.createElement('div');
+
+                if (playerPoints > computerPoints) {
+                    div.textContent = 'YOU WIN THE ROUND! CONGRATULATIONS!';
+                } else if (playerPoints == computerPoints) {
+                    div.textContent = 'YOUR EVEN!';
+                } else {
+                    div.textContent = 'THE COMPUTER WINS THE ROUND!';
+                };
+                content.appendChild(div);
             };
         };
     });
